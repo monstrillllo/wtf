b2065a7b3a828ce42603c3abf87f70f95d90cc7f
Evgeniy <evgeniy27112000@gmail.com>
2020-04-26 20:22:09 +0300

    Can make table and generate 1 Word in rand box

--- src/com/company/GroupSix.java
+++ src/com/company/GroupSix.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
+
 public class GroupSix extends JPanel {
     int ex = 0;
     JTextField TextOne;
@@ -15,7 +17,7 @@ public class GroupSix extends JPanel {
         setBackground(Color.blue);
         TextOne = new JTextField(12);
         TextTwo = new JTextField(12);
-       // TableSix = new JTable(2,2);
+        TableSix = new JTable(2,2);
         JLabel label = new JLabel("Group Six");
         JButton sixButton1 = new JButton("first");
         sixButton1.addActionListener(new buttonOne());
@@ -37,7 +39,20 @@ public class GroupSix extends JPanel {
 
         @Override
         public void actionPerformed(ActionEvent actionEvent) {
-
+            if(ex==0){int x = Integer.parseInt(TextOne.getText());
+                int y = Integer.parseInt(TextTwo.getText());
+                remove(TableSix);
+                revalidate();
+                TableSix = new JTable(x,y);
+                add(TableSix);
+                revalidate();
+                repaint();
+                ex++;
+            }else{
+                ex=0;
+                Random randValue = new Random();
+                TableSix.setValueAt("Random word"+ ex, randValue.nextInt(TableSix.getRowCount()) , randValue.nextInt(TableSix.getColumnCount()) );
+            }
             
         }
     }
@@ -46,9 +61,7 @@ public class GroupSix extends JPanel {
 
         @Override
         public void actionPerformed(ActionEvent actionEvent) {
-            int x = Integer.parseInt(TextOne.getText());
-            int y = Integer.parseInt(TextTwo.getText());
-            TableSix = new JTable(x,y);
+
         }
     }
 
@@ -59,4 +72,5 @@ public class GroupSix extends JPanel {
 
         }
     }
+
 }
